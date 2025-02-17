544167341369ed53e4abd2e03a08d60553ecc0a9
Mario Lim <44302894+JavascriptAddict@users.noreply.github.com>
2025-02-18 01:17:51 +0800

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,18 +1,18 @@
 
 # Activate the Python virtual environment
-& "SkillsReadyTalentFutureAdamKhooAcademy\venv\Scripts\Activate.ps1"
+& "SRTFAKA\venv\Scripts\Activate.ps1"
 
 # Check if the environment is activated
 if ($?) {
     Write-Host "Virtual environment activated successfully." -ForegroundColor Green
 
     # Start the first server in a new PowerShell process
-    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m uvicorn SkillsReadyTalentFutureAdamKhooAcademy.apiGateway.main:app --host 0.0.0.0 --port 80"
-    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SkillsReadyTalentFutureAdamKhooAcademy.accountService.main"
-    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SkillsReadyTalentFutureAdamKhooAcademy.courseService.main"
-    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SkillsReadyTalentFutureAdamKhooAcademy.assessmentService.main"
-    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SkillsReadyTalentFutureAdamKhooAcademy.jobService.main"
-    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SkillsReadyTalentFutureAdamKhooAcademy.certificateService.main"
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m uvicorn SRTFAKA.apiGateway.main:app --host 0.0.0.0 --port 80"
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SRTFAKA.accountService.main"
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SRTFAKA.courseService.main"
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SRTFAKA.assessmentService.main"
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SRTFAKA.jobService.main"
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "& 'python' -m SRTFAKA.certificateService.main"
 } else {
     Write-Host "Failed to activate the virtual environment." -ForegroundColor Red
 }
