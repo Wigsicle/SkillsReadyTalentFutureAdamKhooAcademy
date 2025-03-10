0e81a61d48a6d77931b2066f618b742799073d9e
Mario Lim <44302894+JavascriptAddict@users.noreply.github.com>
2025-03-11 00:28:41 +0800

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,25 +4,40 @@ import "../styles.css"; // Ensure you import the CSS file
 
 function Home() {
     return (
-        <div className="container">
+        <div className="container-fluid">
             <h1>Welcome to SkillsReadyTalentFutureAdamKhooAcademy!</h1>
             <p>Learn new skills, take assessments, earn certificates, and apply for jobs – all in one place!</p>
 
-            <div className="grid">
-                <Link to="/courses" className="card">
-                    <h2> Browse Courses</h2>
-                    <p>Start your journey now to be next best employee.</p>
-                </Link>
+            <div className="container-fluid w-100 mt-5">
 
-                <Link to="/assessments" className="card">
-                    <h2> Take an Assessment</h2>
-                    <p>Get certified upon successful completion.</p>
-                </Link>
+            <div class="p-3 mb-4 bg-body-tertiary rounded-3">
+                <div class="container-fluid py-5">
+                    <Link to="/courses" className="text-dark">
+                        <h2 class="display-6 fw-bold">Browse Courses</h2>
+                    </Link>
+                <p class="col-md-8 fs-4">Start your journey now to be next best employee.</p>
+                </div>
+            </div>
 
-                <Link to="/job-portal" className="card">
-                    <h2> Apply for Jobs</h2>
-                    <p>Use your certificates to apply for jobs.</p>
+            <div class="p-3 mb-4 bg-body-tertiary rounded-3">
+                <div class="container-fluid py-5">
+                <Link to="/assessments" className="text-dark">
+                    <h2 class="display-6 fw-bold">Take an Assessment</h2>
                 </Link>
+                <p class="col-md-8 fs-4">Get certified upon successful completion.</p>
+                </div>
+            </div>
+               
+            <div class="p-3 mb-4 bg-body-tertiary rounded-3">
+                <div class="container-fluid py-5">
+                <Link to="/job-portal" className="text-dark">
+                    <h2 class="display-6 fw-bold">Apply for Jobs</h2>
+                </Link>
+                <p class="col-md-8 fs-4">Use your certificates to apply for jobs.</p>
+                </div>
+            </div>
+               
+            
             </div>
         </div>
     );
